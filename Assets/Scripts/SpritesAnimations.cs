082538f73cd591097e4805dcd183deacc9e58970
thunderfreeze <you@example.comyohann.tonnerre@gmail,com>
2020-09-29 15:58:27 +0200

    :sparkles: ajout des deplacements a peu pres

---
+++ Assets/Scripts/SpritesAnimations.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpritesAnimations : MonoBehaviour
+{
+
+    public Animator animator;
+   
+    void Update()
+    {
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+    }
+}
