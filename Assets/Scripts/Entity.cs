b27ccdad070e1ab5361dcb3b9a7f2baba974349e
romaric-g <romaric.gauzi1@gmail.com>
2020-10-01 10:44:01 +0200

    weapons damage and distance

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    [SerializeField] private double maxHealth;
+    private double health;
+
+    public void Start()
+    {
+        health = maxHealth;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Damage(double damage)
+    {
+        Debug.Log("I have " + health + " <3");
+        this.health = this.health - damage;
+    }
+}
--- Assets/Scripts/Weapons/Stick.cs
+++ Assets/Scripts/Weapons/Stick.cs
@@ -6,7 +6,24 @@ public class Stick : Weapon
 {
     public override void shoot()
     {
-        
+        Debug.Log("shoot stick");
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+        RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+        if (hit.collider != null)
+        {
+            if (hit.collider.tag == "Enemy")
+            {
+                Debug.Log(getDistance(hit.collider.transform.position));
+                if (getDistance(hit.collider.transform.position) < getReach())
+                {
+                    Entity entity = hit.collider.GetComponent<Entity>();
+                    entity.Damage(getDamage());
+                }
+            }
+        }
     }
 
 }
