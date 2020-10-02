24b61b08b039bd5b97e78d4c1b2efc853a1d2894
romaric-g <romaric.gauzi1@gmail.com>
2020-10-02 13:20:26 +0200

    Add mapv1 + counter + debut enu

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    [SerializeField] private int defaultValue;
+    [SerializeField] private UnityEngine.UI.Text text;
+    private int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.count = defaultValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (text != null)
+        {
+            text.text = count.ToString();
+        }
+    }
+
+    public void SetCount(int count)
+    {
+        this.count = count;
+    }
+
+    public int GetCount()
+    {
+        return this.count;
+    }
+
+
+}
